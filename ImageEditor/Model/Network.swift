ebd1b7f92384df46469a5607e11e2b51166aa474
Yazid Louda <72033741+yazidlouda@users.noreply.github.com>
2021-11-10 14:57:59 -0600

    Fixed upload urls for images

--- ImageEditor/Model/Network.swift
+++ ImageEditor/Model/Network.swift
@@ -12,12 +12,22 @@ import Alamofire
 protocol ImageDataDelegate {
     func didUpdateImages(allImages : [ImageModel])
 }
+
+protocol ImageUploadURLDataDelegate {
+    func didUpdateImageUploadURLs(allImages : ImageUploadURL)
+}
+
 struct NetworkHandler{
     var imageDelegate: ImageDataDelegate?
+    var imageUploadURLDelegate : ImageUploadURLDataDelegate?
     let urlString = "https://eulerity-hackathon.appspot.com/image"
+    let imageUploadURLString = "https://eulerity-hackathon.appspot.com/upload"
     func GetAllImages()  {
         performRequest(urlString: urlString)
+        performRequestForImageUploadURL(urlString: imageUploadURLString)
+       
     }
+    
     func performRequest(urlString: String) {
         // create URL
         if let url = URL(string: urlString){
@@ -32,6 +42,27 @@ struct NetworkHandler{
                 if let safeData = data {
                     if let image = self.parseJSON(ImageData: safeData){
                         self.imageDelegate?.didUpdateImages(allImages: image)
+                    }
+                }
+            }
+            //Start the task
+            task.resume()
+        }
+    }
+    func performRequestForImageUploadURL(urlString: String) {
+        // create URL
+        if let url = URL(string: urlString){
+        //Create URLSession
+        let session = URLSession(configuration: .default)
+        //Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let image = self.parseImageUploadURLJSON(ImageData: safeData){
+                        self.imageUploadURLDelegate?.didUpdateImageUploadURLs(allImages: image)
                     }
                 }
             }
@@ -58,4 +89,21 @@ struct NetworkHandler{
         }
     }
 
+    func parseImageUploadURLJSON(ImageData: Data) -> ImageUploadURL? {
+        
+        let decoder = JSONDecoder()
+        do{
+           let decodedData = try decoder.decode(ImageUpload.self, from: ImageData)
+            
+                let url = decodedData.url
+                let images = ImageUploadURL(url: url)
+                // images.append(ImageUploadURL(url: url!))
+           
+            return images
+        } catch{
+            print("failed with error:",error)
+            
+            return nil
+        }
+    }
 }
